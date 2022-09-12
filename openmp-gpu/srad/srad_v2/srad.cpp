53190ff362e7e7a34b6f4113eb058fceb3056156
Anjia Wang <anjiawang@gmail.com>
2022-09-12 14:46:42 -0400

    Create srad_v2 OpenMP GPU version based on its official CPU version

--- openmp-gpu/srad/srad_v2/srad.cpp
+++ openmp-gpu/srad/srad_v2/srad.cpp
@@ -1,250 +1,218 @@
 // srad.cpp : Defines the entry point for the console application.
-//
 
-//#define OUTPUT
-
-
-#define OPEN
-#define	ITERATION
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_TEAMS 256
+#define NUM_THREADS 1024
 
 void random_matrix(float *I, int rows, int cols);
 
-void usage(int argc, char **argv)
-{
-	fprintf(stderr, "Usage: %s <rows> <cols> <y1> <y2> <x1> <x2> <lamda> <no. of iter>\n", argv[0]);
-	fprintf(stderr, "\t<rows>   - number of rows\n");
-	fprintf(stderr, "\t<cols>    - number of cols\n");
-	fprintf(stderr, "\t<y1> 	 - y1 value of the speckle\n");
-	fprintf(stderr, "\t<y2>      - y2 value of the speckle\n");
-	fprintf(stderr, "\t<x1>       - x1 value of the speckle\n");
-	fprintf(stderr, "\t<x2>       - x2 value of the speckle\n");
-	fprintf(stderr, "\t<lamda>   - lambda (0,1)\n");
-	fprintf(stderr, "\t<no. of iter>   - number of iterations\n");
-	
-	exit(1);
+void usage(int argc, char **argv) {
+  fprintf(stderr,
+          "Usage: %s <rows> <cols> <y1> <y2> <x1> <x2> <no. of threads><lamda> "
+          "<no. of iter>\n",
+          argv[0]);
+  fprintf(stderr, "\t<rows>   - number of rows\n");
+  fprintf(stderr, "\t<cols>    - number of cols\n");
+  fprintf(stderr, "\t<y1> 	 - y1 value of the speckle\n");
+  fprintf(stderr, "\t<y2>      - y2 value of the speckle\n");
+  fprintf(stderr, "\t<x1>       - x1 value of the speckle\n");
+  fprintf(stderr, "\t<x2>       - x2 value of the speckle\n");
+  fprintf(stderr, "\t<no. of threads>  - no. of threads\n");
+  fprintf(stderr, "\t<lamda>   - lambda (0,1)\n");
+  fprintf(stderr, "\t<no. of iter>   - number of iterations\n");
+
+  exit(1);
 }
 
-int main(int argc, char* argv[])
-{   
-	int rows, cols, size_I, size_R, niter = 10, iter, k;
-    float *I, *J, q0sqr, sum, sum2, tmp, meanROI,varROI ;
-	float Jc, G2, L, num, den, qsqr;
-	int *iN,*iS,*jE,*jW;
-	float *dN,*dS,*dW,*dE;
-	int r1, r2, c1, c2;
-	float cN,cS,cW,cE;
-	float *c, D;
-	float lambda;
-	int i, j;
+int main(int argc, char *argv[]) {
+  int rows = 0, cols = 0, size_I, size_R, niter = 10, iter, k;
+  float *I, *J, q0sqr, sum, sum2, tmp, meanROI, varROI;
+  float Jc, G2, L, num, den, qsqr;
+  int *iN, *iS, *jE, *jW;
+  float *dN, *dS, *dW, *dE;
+  int r1 = 0, r2 = 0, c1 = 0, c2 = 0;
+  float cN, cS, cW, cE;
+  float *c, D;
+  float lambda;
+  int i, j;
 
-	if (argc == 9)
-	{
-		rows = atoi(argv[1]); //number of rows in the domain
-		cols = atoi(argv[2]); //number of cols in the domain
-		if ((rows%16!=0) || (cols%16!=0)){
-			fprintf(stderr, "rows and cols must be multiples of 16\n");
-			exit(1);
-		}
-		r1   = atoi(argv[3]); //y1 position of the speckle
-		r2   = atoi(argv[4]); //y2 position of the speckle
-		c1   = atoi(argv[5]); //x1 position of the speckle
-		c2   = atoi(argv[6]); //x2 position of the speckle
-		lambda = atof(argv[7]); //Lambda value
-		niter = atoi(argv[8]); //number of iterations
-	}
-    else{
-		usage(argc, argv);
+  if (argc == 9) {
+    rows = atoi(argv[1]); // number of rows in the domain
+    cols = atoi(argv[2]); // number of cols in the domain
+    if ((rows % 16 != 0) || (cols % 16 != 0)) {
+      fprintf(stderr, "rows and cols must be multiples of 16\n");
+      exit(1);
     }
+    r1 = atoi(argv[3]);     // y1 position of the speckle
+    r2 = atoi(argv[4]);     // y2 position of the speckle
+    c1 = atoi(argv[5]);     // x1 position of the speckle
+    c2 = atoi(argv[6]);     // x2 position of the speckle
+    lambda = atof(argv[7]); // Lambda value
+    niter = atoi(argv[8]);  // number of iterations
+  } else {
+    usage(argc, argv);
+  }
 
+  size_I = cols * rows;
+  size_R = (r2 - r1 + 1) * (c2 - c1 + 1);
 
-	size_I = cols * rows;
-    size_R = (r2-r1+1)*(c2-c1+1);   
+  I = (float *)malloc(size_I * sizeof(float));
+  J = (float *)malloc(size_I * sizeof(float));
+  c = (float *)malloc(sizeof(float) * size_I);
 
-	I = (float *)malloc( size_I * sizeof(float) );
-    J = (float *)malloc( size_I * sizeof(float) );
-	c  = (float *)malloc(sizeof(float)* size_I) ;
+  iN = (int *)malloc(sizeof(unsigned int *) * rows);
+  iS = (int *)malloc(sizeof(unsigned int *) * rows);
+  jW = (int *)malloc(sizeof(unsigned int *) * cols);
+  jE = (int *)malloc(sizeof(unsigned int *) * cols);
 
-    iN = (int *)malloc(sizeof(unsigned int*) * rows) ;
-    iS = (int *)malloc(sizeof(unsigned int*) * rows) ;
-    jW = (int *)malloc(sizeof(unsigned int*) * cols) ;
-    jE = (int *)malloc(sizeof(unsigned int*) * cols) ;    
+  dN = (float *)malloc(sizeof(float) * size_I);
+  dS = (float *)malloc(sizeof(float) * size_I);
+  dW = (float *)malloc(sizeof(float) * size_I);
+  dE = (float *)malloc(sizeof(float) * size_I);
 
+  for (int i = 0; i < rows; i++) {
+    iN[i] = i - 1;
+    iS[i] = i + 1;
+  }
+  for (int j = 0; j < cols; j++) {
+    jW[j] = j - 1;
+    jE[j] = j + 1;
+  }
+  iN[0] = 0;
+  iS[rows - 1] = rows - 1;
+  jW[0] = 0;
+  jE[cols - 1] = cols - 1;
 
-	dN = (float *)malloc(sizeof(float)* size_I) ;
-    dS = (float *)malloc(sizeof(float)* size_I) ;
-    dW = (float *)malloc(sizeof(float)* size_I) ;
-    dE = (float *)malloc(sizeof(float)* size_I) ;    
-    
-//#pragma acc data create(iN[0:rows],iS[0:rows],jW[0:cols],jE[0:cols]) \
-//    create(dN[0:size_I],dS[0:size_I],dW[0:size_I],dE[0:size_I],c[0:size_I]) \
-//    create(I[0:size_I]) copyout(J[0:size_I])
-#pragma omp target data map(alloc:iN[0:rows],iS[0:rows],jW[0:cols],jE[0:cols]) \
-    map(alloc:dN[0:size_I],dS[0:size_I],dW[0:size_I],dE[0:size_I],c[0:size_I]) \
-    map(alloc:I[0:size_I]) map(from:J[0:size_I])
-{
-    //#pragma acc parallel loop
-    #pragma omp target teams distribute parallel for 
-    for (int i=0; i< rows; i++) {
-        iN[i] = i-1;
-        iS[i] = i+1;
-    }
-    //#pragma acc parallel loop
-    #pragma omp target teams distribute parallel for 
-    for (int j=0; j< cols; j++) {
-        jW[j] = j-1;
-        jE[j] = j+1;
-    }
-    //#pragma acc kernels
-    #pragma omp target
-    {
-    iN[0]    = 0;
-    iS[rows-1] = rows-1;
-    jW[0]    = 0;
-    jE[cols-1] = cols-1;
-    }
-	
-	printf("Randomizing the input matrix\n");
+  printf("Randomizing the input matrix\n");
 
-    random_matrix(I, rows, cols);
-    //#pragma acc update device(I[0:size_I])
-    #pragma omp target update to(I[0:size_I])
+  random_matrix(I, rows, cols);
 
-    //#pragma acc parallel loop
-    #pragma omp target teams distribute parallel for 
-    for (k = 0;  k < size_I; k++ ) {
-     	J[k] = (float)exp(I[k]) ;
-    }
-   
-	printf("Start the SRAD main loop\n");
+  for (k = 0; k < size_I; k++) {
+    J[k] = (float)exp(I[k]);
+  }
 
-#ifdef ITERATION
-	for (iter=0; iter< niter; iter++){
-#endif        
-		sum=0; sum2=0;     
-		//#pragma acc parallel loop collapse(2) reduction(+:sum,sum2)
-    		#pragma omp target teams distribute parallel for collapse(2) reduction(+:sum,sum2)
-		for (i=r1; i<=r2; i++) {
-            for (j=c1; j<=c2; j++) {
-                tmp   = J[i * cols + j];
-                sum  += tmp ;
-                sum2 += tmp*tmp;
-            }
+  printf("Start the SRAD main loop\n");
+
+#pragma omp target data map(tofrom                                             \
+                            : J [0:size_I])                                    \
+    map(to                                                                     \
+        : iN [0:rows], iS [0:rows], jW [0:cols], jE [0:cols], dN [0:size_I],   \
+          dS [0:size_I], dW [0:size_I], dE [0:size_I], c [0:size_I])
+  {
+    for (iter = 0; iter < niter; iter++) {
+      sum = 0;
+      sum2 = 0;
+      for (i = r1; i <= r2; i++) {
+        for (j = c1; j <= c2; j++) {
+          tmp = J[i * cols + j];
+          sum += tmp;
+          sum2 += tmp * tmp;
         }
-        meanROI = sum / size_R;
-        varROI  = (sum2 / size_R) - meanROI*meanROI;
-        q0sqr   = varROI / (meanROI*meanROI);
-		
+      }
+      meanROI = sum / size_R;
+      varROI = (sum2 / size_R) - meanROI * meanROI;
+      q0sqr = varROI / (meanROI * meanROI);
 
-        //#pragma acc parallel loop collapse(2)
-    	#pragma omp target teams distribute parallel for collapse(2)
-		for (int i = 0 ; i < rows ; i++) {
-            for (int j = 0; j < cols; j++) { 
-		
-				float dNk, dSk, dWk, dEk, ck;
-				
-				k = i * cols + j;
-				Jc = J[k];
- 
-				// directional derivates
-                dNk = J[iN[i] * cols + j] - Jc;
-                dSk = J[iS[i] * cols + j] - Jc;
-                dWk = J[i * cols + jW[j]] - Jc;
-                dEk = J[i * cols + jE[j]] - Jc;
-			
-                G2 = (dNk*dNk + dSk*dSk 
-                    + dWk*dWk + dEk*dEk) / (Jc*Jc);
+#pragma omp target teams distribute parallel for shared(                       \
+    J, dN, dS, dW, dE, c, rows, cols, iN, iS, jW,                              \
+    jE) private(i, j, k, Jc, G2, L, num, den, qsqr) num_teams(NUM_TEAMS)       \
+    num_threads(NUM_THREADS)
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
 
-   		        L = (dNk + dSk + dWk + dEk) / Jc;
+          k = i * cols + j;
+          Jc = J[k];
 
-				num  = (0.5*G2) - ((1.0/16.0)*(L*L)) ;
-                den  = 1 + (.25*L);
-                qsqr = num/(den*den);
- 
-                // diffusion coefficent (equ 33)
-                den = (qsqr-q0sqr) / (q0sqr * (1+q0sqr)) ;
-                ck = 1.0 / (1.0+den) ;
-                
-                // saturate diffusion coefficent
-                if (ck < 0) {c[k] = 0;}
-                else if (ck >= 1) {c[k] = 1;}
-                
-                dN[k] = dNk, dS[k] = dSk, dW[k] = dWk, dE[k] = dEk;
-   
-			}
-    	}
+          // directional derivates
+          dN[k] = J[iN[i] * cols + j] - Jc;
+          dS[k] = J[iS[i] * cols + j] - Jc;
+          dW[k] = J[i * cols + jW[j]] - Jc;
+          dE[k] = J[i * cols + jE[j]] - Jc;
 
-    	//#pragma acc parallel loop collapse(2)
-    	#pragma omp target teams distribute parallel for collapse(2)
-		for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {        
+          G2 = (dN[k] * dN[k] + dS[k] * dS[k] + dW[k] * dW[k] + dE[k] * dE[k]) /
+               (Jc * Jc);
 
-                // current index
-                k = i * cols + j;
-                
-                // diffusion coefficent
-					cN = c[k];
-					cS = c[iS[i] * cols + j];
-					cW = c[k];
-					cE = c[i * cols + jE[j]];
+          L = (dN[k] + dS[k] + dW[k] + dE[k]) / Jc;
 
-                // divergence (equ 58)
-                D = cN * dN[k] + cS * dS[k] + cW * dW[k] + cE * dE[k];
-                
-                // image update (equ 61)
-                J[k] = J[k] + 0.25*lambda*D;
-            }
-	     }
+          num = (0.5 * G2) - ((1.0 / 16.0) * (L * L));
+          den = 1 + (.25 * L);
+          qsqr = num / (den * den);
 
-#ifdef ITERATION
-	}
-#endif
+          // diffusion coefficent (equ 33)
+          den = (qsqr - q0sqr) / (q0sqr * (1 + q0sqr));
+          c[k] = 1.0 / (1.0 + den);
 
-} /* end acc data */
+          // saturate diffusion coefficent
+          if (c[k] < 0) {
+            c[k] = 0;
+          } else if (c[k] > 1) {
+            c[k] = 1;
+          }
+        }
+      }
+#pragma omp target teams distribute parallel for shared(                       \
+    J, c, rows, cols, lambda) private(i, j, k, D, cS, cN, cW, cE)              \
+    num_teams(NUM_TEAMS) num_threads(NUM_THREADS)
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+
+          // current index
+          k = i * cols + j;
+
+          // diffusion coefficent
+          cN = c[k];
+          cS = c[iS[i] * cols + j];
+          cW = c[k];
+          cE = c[i * cols + jE[j]];
+
+          // divergence (equ 58)
+          D = cN * dN[k] + cS * dS[k] + cW * dW[k] + cE * dE[k];
+
+          // image update (equ 61)
+          J[k] = J[k] + 0.25 * lambda * D;
+        }
+      }
+    } // iterations end
+  }   // target data region ends
 
 #ifdef OUTPUT
-	  for( int i = 0 ; i < rows ; i++){
-		for ( int j = 0 ; j < cols ; j++){
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
 
-         printf("%.5f ", J[i * cols + j]); 
-    
-		}
-         printf("\n"); 
-   }
-#endif 
+      printf("%.5f ", J[i * cols + j]);
+    }
+    printf("\n");
+  }
+#endif
 
-	printf("Computation Done\n");
+  printf("Computation Done\n");
 
-	free(I);
-	free(J);
-	free(iN); free(iS); free(jW); free(jE);
-    free(dN); free(dS); free(dW); free(dE);
+  free(I);
+  free(J);
+  free(iN);
+  free(iS);
+  free(jW);
+  free(jE);
+  free(dN);
+  free(dS);
+  free(dW);
+  free(dE);
 
-	free(c);
-	return 0;
+  free(c);
+  return 0;
 }
 
+void random_matrix(float *I, int rows, int cols) {
 
+  srand(7);
 
-
-void random_matrix(float *I, int rows, int cols){
-
-	srand(7);
-	
-	for( int i = 0 ; i < rows ; i++){
-		for ( int j = 0 ; j < cols ; j++){
-		 I[i * cols + j] = rand()/(float)RAND_MAX ;
-		 #ifdef OUTPUT
-         //printf("%g ", I[i * cols + j]); 
-         #endif 
-		}
-		 #ifdef OUTPUT
-         //printf("\n"); 
-         #endif 
-	}
-
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      I[i * cols + j] = rand() / (float)RAND_MAX;
+    }
+  }
 }
-
