56183f66148b2af6c257f2c77e0d4d611a08821a
Anjia Wang <anjiawang@gmail.com>
2022-09-12 00:39:08 -0400

    Create b+tree OpenACC version based on its OpenMP GPU version
    The output is slightly different from the OpenMP GPU version, it will be addressed later in a separate ticket.
    
    The original OpenACC version doesn't work, so it's abandoned.

--- openacc/b+tree/kernel/kernel_cpu.c
+++ openacc/b+tree/kernel/kernel_cpu.c
@@ -1,153 +1,132 @@
-// #ifdef __cplusplus
-// extern "C" {
-// #endif
-
 //========================================================================================================================================================================================================200
 //	DEFINE/INCLUDE
 //========================================================================================================================================================================================================200
+
+#define NUM_TEAMS 256
+#define NUM_THREADS 1024
 
 //======================================================================================================================================================150
 //	LIBRARIES
 //======================================================================================================================================================150
 
-#include <stdlib.h>									// (in directory known to compiler)			needed by malloc
-#include <stdio.h>									// (in directory known to compiler)			needed by printf, stderr
+#include <stdio.h> // (in directory known to compiler)			needed by printf, stderr
+#include <stdlib.h> // (in directory known to compiler)			needed by malloc
 
 //======================================================================================================================================================150
 //	COMMON
 //======================================================================================================================================================150
 
-#include "../common.h"								// (in directory provided here)
+#include "../common.h" // (in directory provided here)
 
 //======================================================================================================================================================150
 //	UTILITIES
 //======================================================================================================================================================150
 
-#include "../util/timer/timer.h"					// (in directory provided here)
+#include "../util/timer/timer.h" // (in directory provided here)
 
 //========================================================================================================================================================================================================200
 //	KERNEL_CPU FUNCTION
 //========================================================================================================================================================================================================200
 
-void 
-kernel_cpu(	int cores_arg,
+void kernel_cpu(int cores_arg,
 
-			record *records,
-			knode *knodes,
-			long knodes_elem,
+                record *records, knode *knodes, long knodes_elem,
 
-			int order,
-			long maxheight,
-			int count,
+                int order, long maxheight, int count,
 
-			long *currKnode,
-			long *offset,
-			int *keys,
-			record *ans)
-{
+                long *currKnode, long *offset, int *keys, record *ans) {
 
-	//======================================================================================================================================================150
-	//	Variables
-	//======================================================================================================================================================150
+  //======================================================================================================================================================150
+  //	Variables
+  //======================================================================================================================================================150
 
-	// timer
-	long long time0;
-	long long time1;
-	long long time2;
+  // timer
+  long long time0;
+  long long time1;
+  long long time2;
 
-	time0 = get_time();
+  time0 = get_time();
 
-	//======================================================================================================================================================150
-	//	MCPU SETUP
-	//======================================================================================================================================================150
+  //======================================================================================================================================================150
+  //	MCPU SETUP
+  //======================================================================================================================================================150
 
-	int threadsPerBlock;
-	threadsPerBlock = order < 1024 ? order : 1024;
+  int threadsPerBlock;
+  threadsPerBlock = order < 1024 ? order : 1024;
 
-	time1 = get_time();
+  time1 = get_time();
 
-	//======================================================================================================================================================150
-	//	PROCESS INTERACTIONS
-	//======================================================================================================================================================150
+  //======================================================================================================================================================150
+  //	PROCESS INTERACTIONS
+  //======================================================================================================================================================150
 
-	// private thread IDs
-	int thid;
-	int bid;
-	int i;
+  // private thread IDs
+  int thid;
+  int bid;
+  int i;
 
-	// process number of querries
-	#pragma acc kernels
-	for(bid = 0; bid < count; bid++){
+// process number of querries
+#pragma acc parallel loop private(i, thid) num_gangs(NUM_TEAMS) num_workers(1) \
+    vector_length(NUM_THREADS) copyin(currKnode [0:count], offset [0:count])   \
+        copyin(knodes [0:knodes_elem], keys [0:count]) copy(ans [0:count])     \
+            present(records)
+  for (bid = 0; bid < count; bid++) {
 
-		// process levels of the tree
-		for(i = 0; i < maxheight; i++){
+    // process levels of the tree
+    for (i = 0; i < maxheight; i++) {
 
-			// process all leaves at each level
-			for(thid = 0; thid < threadsPerBlock; thid++){
+      // process all leaves at each level
+      for (thid = 0; thid < threadsPerBlock; thid++) {
 
-				// if value is between the two keys
-				if((knodes[currKnode[bid]].keys[thid]) <= keys[bid] && (knodes[currKnode[bid]].keys[thid+1] > keys[bid])){
-					// this conditional statement is inserted to avoid crush due to but in original code
-					// "offset[bid]" calculated below that addresses knodes[] in the next iteration goes outside of its bounds cause segmentation fault
-					// more specifically, values saved into knodes->indices in the main function are out of bounds of knodes that they address
-					if(knodes[offset[bid]].indices[thid] < knodes_elem){
-						offset[bid] = knodes[offset[bid]].indices[thid];
-					}
-				}
+        // if value is between the two keys
+        if ((knodes[currKnode[bid]].keys[thid]) <= keys[bid] &&
+            (knodes[currKnode[bid]].keys[thid + 1] > keys[bid])) {
+          // this conditional statement is inserted to avoid crush due to but in
+          // original code "offset[bid]" calculated below that addresses
+          // knodes[] in the next iteration goes outside of its bounds cause
+          // segmentation fault more specifically, values saved into
+          // knodes->indices in the main function are out of bounds of knodes
+          // that they address
+          if (knodes[offset[bid]].indices[thid] < knodes_elem) {
+            offset[bid] = knodes[offset[bid]].indices[thid];
+          }
+        }
+      }
 
-			}
-		}
-	}
+      // set for next tree level
+      currKnode[bid] = offset[bid];
+    }
 
-	#pragma acc kernels
-	for(bid = 0; bid < count; bid++){
+    // At this point, we have a candidate leaf node which may contain
+    // the target record.  Check each key to hopefully find the record
+    //  process all leaves at each level
+    for (thid = 0; thid < threadsPerBlock; thid++) {
 
-		// process levels of the tree
-		for(i = 0; i < maxheight; i++){
+      if (knodes[currKnode[bid]].keys[thid] == keys[bid]) {
+        ans[bid].value = records[knodes[currKnode[bid]].indices[thid]].value;
+      }
+    }
+  }
 
-			// set for next tree level
-			currKnode[bid] = offset[bid];
+  time2 = get_time();
 
-		}
-	}
+  //======================================================================================================================================================150
+  //	DISPLAY TIMING
+  //======================================================================================================================================================150
 
-	#pragma acc kernels
-	for(bid = 0; bid < count; bid++){
+  printf("Time spent in different stages of CPU/MCPU KERNEL:\n");
 
-		//At this point, we have a candidate leaf node which may contain
-		//the target record.  Check each key to hopefully find the record
-		// process all leaves at each level
-		for(thid = 0; thid < threadsPerBlock; thid++){
+  printf("%15.12f s, %15.12f %% : MCPU: SET DEVICE\n",
+         (float)(time1 - time0) / 1000000,
+         (float)(time1 - time0) / (float)(time2 - time0) * 100);
+  printf("%15.12f s, %15.12f %% : CPU/MCPU: KERNEL\n",
+         (float)(time2 - time1) / 1000000,
+         (float)(time2 - time1) / (float)(time2 - time0) * 100);
 
-			if(knodes[currKnode[bid]].keys[thid] == keys[bid]){
-				ans[bid].value = records[knodes[currKnode[bid]].indices[thid]].value;
-			}
-
-		}
-
-	}
-
-	time2 = get_time();
-
-	//======================================================================================================================================================150
-	//	DISPLAY TIMING
-	//======================================================================================================================================================150
-
-	printf("Time spent in different stages of CPU/MCPU KERNEL:\n");
-
-	printf("%15.12f s, %15.12f % : MCPU: SET DEVICE\n",					(float) (time1-time0) / 1000000, (float) (time1-time0) / (float) (time2-time0) * 100);
-	printf("%15.12f s, %15.12f % : CPU/MCPU: KERNEL\n",					(float) (time2-time1) / 1000000, (float) (time2-time1) / (float) (time2-time0) * 100);
-
-	printf("Total time:\n");
-	printf("%.12f s\n", 												(float) (time2-time0) / 1000000);
-
+  printf("Total time:\n");
+  printf("%.12f s\n", (float)(time2 - time0) / 1000000);
 }
 
 //========================================================================================================================================================================================================200
 //	END
 //========================================================================================================================================================================================================200
-
-// #ifdef __cplusplus
-// }
-// #endif
-
