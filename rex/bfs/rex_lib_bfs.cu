05c8c72deff4e4069b0d883c940782fb22c9174f
Anjia Wang <anjiawang@gmail.com>
2023-01-03 17:21:15 -0500

    Regenerate the REX version of bfs from its C version

--- rex/bfs/rex_lib_bfs.cu
+++ rex/bfs/rex_lib_bfs.cu
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +7,7 @@
 #define NUM_TEAMS 256
 #define NUM_THREADS 1024
 #include "rex_nvidia.h" 
-extern ::FILE *fp;
+extern FILE *fp;
 // Structure to hold a node information
 
 struct Node 
@@ -26,15 +27,14 @@ int main(int ,char **);
 // Apply BFS on a Graph using CUDA
 ////////////////////////////////////////////////////////////////////////////////
 void BFSGraph(int ,char **);
-__device__ char OUT__2__5397__BFSGraph__151__kernel___exec_mode = 0;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-__global__ void OUT__2__5397__BFSGraph__151__kernel__(int *no_of_nodesp__,bool *stopp__,bool *_dev_h_graph_mask,bool *_dev_h_updating_graph_mask,bool *_dev_h_graph_visited)
+__device__ char OUT__2__4476__BFSGraph__153__kernel___exec_mode = 0;
+
+__global__ void OUT__2__4476__BFSGraph__153__kernel__(int *no_of_nodesp__,_Bool *h_graph_mask,_Bool *h_updating_graph_mask,_Bool *h_graph_visited,_Bool *stopp__)
 {
   int tid;
-  typedef int int64_t;
 {
     int _dev_lower;
     int _dev_upper;
@@ -43,24 +43,23 @@ __global__ void OUT__2__5397__BFSGraph__151__kernel__(int *no_of_nodesp__,bool *
     int _dev_loop_stride;
     int _dev_thread_num = getCUDABlockThreadCount(1);
     int _dev_thread_id = getLoopIndexFromCUDAVariables(1);
-    XOMP_static_sched_init(0,*no_of_nodesp__ - 1,1,1,_dev_thread_num,_dev_thread_id,&_dev_loop_chunk_size,&_dev_loop_sched_index,&_dev_loop_stride);
-    while(XOMP_static_sched_next(&_dev_loop_sched_index,*no_of_nodesp__ - 1,1,_dev_loop_stride,_dev_loop_chunk_size,_dev_thread_num,_dev_thread_id,&_dev_lower,&_dev_upper))
+    XOMP_static_sched_init(0, *no_of_nodesp__ - 1,1,1,_dev_thread_num,_dev_thread_id,&_dev_loop_chunk_size,&_dev_loop_sched_index,&_dev_loop_stride);
+    while(XOMP_static_sched_next(&_dev_loop_sched_index, *no_of_nodesp__ - 1,1,_dev_loop_stride,_dev_loop_chunk_size,_dev_thread_num,_dev_thread_id,&_dev_lower,&_dev_upper))
       for (tid = _dev_lower; tid <= _dev_upper; tid += 1) {
-        if (((int )_dev_h_updating_graph_mask[tid]) == ((int )true)) {
-          _dev_h_graph_mask[tid] = true;
-          _dev_h_graph_visited[tid] = true;
-          *stopp__ = true;
-          _dev_h_updating_graph_mask[tid] = false;
+        if (((int )h_updating_graph_mask[tid]) == 1) {
+          h_graph_mask[tid] = ((_Bool )1);
+          h_graph_visited[tid] = ((_Bool )1);
+           *stopp__ = ((_Bool )1);
+          h_updating_graph_mask[tid] = ((_Bool )0);
         }
       }
   }
 }
-__device__ char OUT__1__5397__BFSGraph__133__kernel___exec_mode = 0;
+__device__ char OUT__1__4476__BFSGraph__135__kernel___exec_mode = 0;
 
-__global__ void OUT__1__5397__BFSGraph__133__kernel__(int *no_of_nodesp__,struct Node *_dev_h_graph_nodes,bool *_dev_h_graph_mask,bool *_dev_h_updating_graph_mask,bool *_dev_h_graph_visited,int *_dev_h_graph_edges,int *_dev_h_cost)
+__global__ void OUT__1__4476__BFSGraph__135__kernel__(int *no_of_nodesp__,struct Node *h_graph_nodes,_Bool *h_graph_mask,_Bool *h_updating_graph_mask,_Bool *h_graph_visited,int *h_graph_edges,int *h_cost)
 {
   int tid;
-  typedef int int64_t;
 {
     int _dev_lower;
     int _dev_upper;
@@ -69,16 +68,16 @@ __global__ void OUT__1__5397__BFSGraph__133__kernel__(int *no_of_nodesp__,struct
     int _dev_loop_stride;
     int _dev_thread_num = getCUDABlockThreadCount(1);
     int _dev_thread_id = getLoopIndexFromCUDAVariables(1);
-    XOMP_static_sched_init(0,*no_of_nodesp__ - 1,1,1,_dev_thread_num,_dev_thread_id,&_dev_loop_chunk_size,&_dev_loop_sched_index,&_dev_loop_stride);
-    while(XOMP_static_sched_next(&_dev_loop_sched_index,*no_of_nodesp__ - 1,1,_dev_loop_stride,_dev_loop_chunk_size,_dev_thread_num,_dev_thread_id,&_dev_lower,&_dev_upper))
+    XOMP_static_sched_init(0, *no_of_nodesp__ - 1,1,1,_dev_thread_num,_dev_thread_id,&_dev_loop_chunk_size,&_dev_loop_sched_index,&_dev_loop_stride);
+    while(XOMP_static_sched_next(&_dev_loop_sched_index, *no_of_nodesp__ - 1,1,_dev_loop_stride,_dev_loop_chunk_size,_dev_thread_num,_dev_thread_id,&_dev_lower,&_dev_upper))
       for (tid = _dev_lower; tid <= _dev_upper; tid += 1) {
-        if (((int )_dev_h_graph_mask[tid]) == ((int )true)) {
-          _dev_h_graph_mask[tid] = false;
-          for (int i = _dev_h_graph_nodes[tid] . starting; i < _dev_h_graph_nodes[tid] . no_of_edges + _dev_h_graph_nodes[tid] . starting; i++) {
-            int id = _dev_h_graph_edges[i];
-            if (!_dev_h_graph_visited[id]) {
-              _dev_h_cost[id] = _dev_h_cost[tid] + 1;
-              _dev_h_updating_graph_mask[id] = true;
+        if (((int )h_graph_mask[tid]) == 1) {
+          h_graph_mask[tid] = ((_Bool )0);
+          for (int i = h_graph_nodes[tid] . starting; i < h_graph_nodes[tid] . no_of_edges + h_graph_nodes[tid] . starting; i++) {
+            int id = h_graph_edges[i];
+            if (!h_graph_visited[id]) {
+              h_cost[id] = h_cost[tid] + 1;
+              h_updating_graph_mask[id] = ((_Bool )1);
             }
           }
         }
