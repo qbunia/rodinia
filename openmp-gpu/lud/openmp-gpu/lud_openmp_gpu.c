5a8075aa0453225253aaa96bac16ea6820d00cfc
Xinyao Yi <yxyict@gmail.com>
2023-01-05 00:47:44 -0500

    Update OpenMP and OpenACC versions for lud. The OpenACC version is a little bit faster.

--- openmp-gpu/lud/openmp-gpu/lud_openmp_gpu.c
+++ openmp-gpu/lud/openmp-gpu/lud_openmp_gpu.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#define NUM_TEAMS 256
-#define NUM_THREADS 1024
 
 void lud_openmp_gpu(float *a, int size)
 {
@@ -9,16 +7,18 @@ void lud_openmp_gpu(float *a, int size)
      #pragma omp target data map(tofrom:a[0:size*size])
      
      for (i=0; i <size; i++){
-         #pragma omp target teams distribute parallel for num_teams(NUM_TEAMS) num_threads(NUM_THREADS)
+         //Here we choose to parallize the second layer of the loop which is faster. 
+         //#pragma omp target teams distribute
+         #pragma omp target teams distribute parallel for
          for (j=i; j <size; j++){
              sum=a[i*size+j];
+             //#pragma omp parallel for reduction(-:sum) num_threads(NUM_THREADS)
              for (k=0; k<i; k++) sum -= a[i*size+k]*a[k*size+j];
              a[i*size+j]=sum;
          }
-         #pragma omp target teams distribute parallel for num_teams(NUM_TEAMS) num_threads(NUM_THREADS)
+         #pragma omp target teams distribute parallel for
          for (j=i+1;j<size; j++){
              sum=a[j*size+i];
-            // #pragma omp for
              for (k=0; k<i; k++) sum -=a[j*size+k]*a[k*size+i];
              a[j*size+i]=sum/a[i*size+i];
          }
