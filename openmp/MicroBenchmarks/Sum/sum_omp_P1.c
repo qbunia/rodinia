88900d73e996cf064f56c5d9995e9cd558e1dac2
Xinyao Yi <yxyict@gmail.com>
2023-10-19 06:14:30 -0400

    Added the openmp versiond for sum

--- openmp/MicroBenchmarks/Sum/sum_omp_P1.c
+++ openmp/MicroBenchmarks/Sum/sum_omp_P1.c
@@ -6,7 +6,6 @@
 #include <math.h>
 #include <string.h>
 #include <sys/timeb.h>
-#include <omp.h>
 
 /* read timer in second */
 double read_timer() {
@@ -44,10 +43,9 @@ void init(REAL A[], int N) {
     }
 }
 
-int num_threads = 2;
-REAL sum(int N, REAL X[], REAL a);
+int num_threads = 1024;
+REAL sum_serial(int N, REAL X[], REAL a);
 REAL sum_omp_parallel_for(int N, REAL X[], REAL a);
-REAL sum_omp_parallel_optimization(int N, REAL X[], REAL a);
 
 int main(int argc, char *argv[]) {
     int N = VECTOR_LENGTH;
@@ -56,8 +54,6 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
     N = atoi(argv[1]);
-    if (argc >=3) num_threads = atoi(argv[2]);
-    omp_set_num_threads(num_threads);
     
     REAL *X = malloc(sizeof(REAL)*N);
 
@@ -66,20 +62,15 @@ int main(int argc, char *argv[]) {
     REAL a = 0.1234;
     /* example run */
     double serial_elapsed = read_timer();
-    REAL serial_result = sum(N, X, a);
+    REAL serial_result = sum_serial(N, X, a);
     serial_elapsed = (read_timer() - serial_elapsed);
-
+    
     double parallel_for_elapsed = read_timer();
     REAL parallel_for_result = sum_omp_parallel_for(N, X, a);
     parallel_for_elapsed = (read_timer() - parallel_for_elapsed);
 
-    double parallel_elapsed = read_timer();
-    REAL parallel_elapsed_result = sum_omp_parallel_optimization(N, X, a);
-    parallel_elapsed = (read_timer() - parallel_elapsed);
-
     log_result("serial", N, num_threads, serial_elapsed);
     log_result("parallel for", N, num_threads, parallel_for_elapsed);
-    log_result("parallel", N, num_threads, parallel_elapsed);
 
     /* you should add the call to each function and time the execution */
     printf("======================================================================================================\n");
@@ -89,13 +80,16 @@ int main(int argc, char *argv[]) {
     printf("------------------------------------------------------------------------------------------------------\n");
     printf("Sum serial:      \t%4f\t%4f\n", serial_elapsed * 1.0e3, 2*N / (1.0e6 * serial_elapsed));
     printf("Sum parallel for:\t%4f\t%4f\n", parallel_for_elapsed * 1.0e3, 2*N / (1.0e6 * parallel_for_elapsed));
-   // printf("Sum parallel:    \t%4f\t%4f\n", parallel_elapsed * 1.0e3, 2*N / (1.0e6 * parallel_elapsed));
+    
+    REAL error = serial_result - parallel_for_result;
+    printf("error:%g\n", error);
+
 
     free(X);
     return 0;
 }
 
-REAL sum(int N, REAL X[], REAL a) {
+REAL sum_serial(int N, REAL X[], REAL a) {
     int i;
     REAL result = 0.0;
     for (i = 0; i < N; ++i)
@@ -115,42 +109,4 @@ REAL sum_omp_parallel_for(int N, REAL X[], REAL a) {
         result += a * X[i];
     }
     return result;
-}
-
-/*
- * Optimized reduction using only OpenMP parallel, and you should implement
- * your own way of reduction
- */
-REAL sum_omp_parallel_optimization(int N, REAL X[], REAL a) {
-
-    REAL total = 0;
-
-    #pragma omp parallel
-    {
-        int chunk_size = N / num_threads;
-        int thread_num = omp_get_thread_num();
-
-        int start = thread_num*chunk_size;
-        int stop = (thread_num+1)*chunk_size;
-
-        if (thread_num == num_threads-1)
-        {
-            int remaining = N % num_threads;
-            stop += remaining;
-        }
-
-        REAL local_result = 0.0;
-        int i;
-        for (i = start; i < stop; ++i)
-        {
-            local_result += a * X[i];
-        }
-
-        #pragma omp critical
-        {
-            total += local_result;
-        }
-    }
-
-    return total;
 }
