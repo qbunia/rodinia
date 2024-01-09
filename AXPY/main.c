4fd7954219e7e53bd3d7ce6cf0aed07227ba47d4
Xinyao Yi <yxyict@gmail.com>
2024-01-09 06:23:29 -0500

    Added the option for full_report

--- AXPY/main.c
+++ AXPY/main.c
@@ -1,17 +1,18 @@
 #include "axpy.h"
 
 int main(int argc, char *argv[]) {
-   if (argc < 2) {
-       fprintf(stderr, "Usage: %s  <n> <num_threads>\n", argv[0]);
+   if (argc < 3) {
+       fprintf(stderr, "Usage: %s <n> <num_threads> [full_report]\n", argv[0]);
        exit(1);
    }
-  
-   int default_N = 1024000000;
+
+   int default_N = 1024000;
    int default_num_threads = 4;
-   int full_report = 1;
-  
-   int N = (argc > 1) ? atoi(argv[1]) : default_N;
-   int num_threads = (argc > 2) ? atoi(argv[2]) : default_num_threads;
+   int default_full_report = 1;
+
+   int N = atoi(argv[1]);
+   int num_threads = atoi(argv[2]);
+   int full_report = (argc > 3) ? atoi(argv[3]) : default_full_report;
 
    
    omp_set_num_threads(num_threads);
@@ -30,9 +31,7 @@ int main(int argc, char *argv[]) {
    
    //for (i=0; i<num_runs+1; i++) axpy_P0(N, Y_base, X, a);
        
-   printf("======================================================================================================\n");
-   printf("\tAXPY: Y[N] = Y[N] + a*X[N], N=%d, %d threads for dist\n", N, num_threads);
-   printf("------------------------------------------------------------------------------------------------------\n");
+
    char device_type[10];
    
    double elapsed_omp_parallel_for = read_timer();
@@ -40,12 +39,17 @@ int main(int argc, char *argv[]) {
    axpy_kernel(N, Y_parallel, X, a);
     
    elapsed_omp_parallel_for = (read_timer() - elapsed_omp_parallel_for);
-               
-
-   printf("------------------------------------------------------------------------------------------------------\n");
-   printf("Performance:\t\t\tRuntime (ms)\t MFLOPS \t\tError (compared to base)\n");
-   printf("------------------------------------------------------------------------------------------------------\n");
-   printf("axpy_kernel:\t\t%4f\t%4f \t\t%g\n", elapsed_omp_parallel_for * 1.0e3, (2.0 * N) / (1.0e6 * elapsed_omp_parallel_for), check(Y_base,Y_parallel, N));
+   if(full_report == 1){
+       printf("======================================================================================================\n");
+       printf("\tAXPY: Y[N] = Y[N] + a*X[N], N=%d, %d threads for dist\n", N, num_threads);
+       printf("------------------------------------------------------------------------------------------------------\n");
+       printf("------------------------------------------------------------------------------------------------------\n");
+       printf("Performance:\t\t\tRuntime (ms)\t MFLOPS \t\tError (compared to base)\n");
+       printf("------------------------------------------------------------------------------------------------------\n");
+       printf("axpy_kernel:\t\t%4f\t%4f \t\t%g\n", elapsed_omp_parallel_for * 1.0e3, (2.0 * N) / (1.0e6 * elapsed_omp_parallel_for), check(Y_base,Y_parallel, N));
+   } else {
+        printf("%4f",elapsed_omp_parallel_for);
+   }
 
    free(Y_base);
    free(Y_parallel);
